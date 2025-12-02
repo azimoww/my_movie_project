4c47b63f1c0cf251ba1f4bada8508ac55242b547
c3sar628 <heyderezimov@gmail.com>
2025-12-02 21:12:37 +0400

    .

---
+++ My-movie/src/main/java/com/movie/dea/controller/MovieController.java
@@ -0,0 +1,63 @@
+package com.movie.dea.controller;
+
+import com.movie.dea.entity.Movie;
+import com.movie.dea.service.MovieService;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/all")
+    public List<Movie> getMovies(){
+        return movieService.getAllMovie();
+    }
+
+    @GetMapping("/title/{title}")
+    public List<Movie> getMoviesByTitle(@PathVariable String title) {
+        return movieService.getAllMovieByTitle(title);
+    }
+
+    @GetMapping("/genre/{genre}")
+    public List<Movie> getMoviesByGenre(@PathVariable String genre) {
+        return movieService.getAllMovieByGenre(genre);
+    }
+
+    @GetMapping("/rating/{rating}")
+    public List<Movie> getAllMoviesByMinRating(@PathVariable Double minRating) {
+        return movieService.getAllMovieByMinRating(minRating);
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable Integer id){
+        return movieService.getMovie(id);
+    }
+
+    @GetMapping("/date/{releaseDate}")
+    public List<Movie> getMovieByDate(@PathVariable LocalDate releaseDate){
+        return movieService.getAllMovieByReleaseDate(releaseDate);
+    }
+
+    @PostMapping("/add")
+    public Movie creatMovie(@RequestBody Movie movie) {
+        return movieService.createMovie(movie);
+    }
+
+    @PutMapping("/update/{id}")
+    public Movie updateMovie(@PathVariable Integer id, @RequestBody Movie movie) {
+        return movieService.updateMovie(id, movie);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteMovie(@PathVariable Integer id) {
+        return movieService.deleteById(id);
+    }
+}
