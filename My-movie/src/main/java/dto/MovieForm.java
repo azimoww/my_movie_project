6e162d4b5706bdb7f6941818d866209aac2b2817
c3sar628 <heyderezimov@gmail.com>
2026-01-17 01:17:14 +0400

    this from

---
+++ My-movie/src/main/java/dto/MovieForm.java
@@ -0,0 +1,74 @@
+package dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+public class MovieForm {
+
+    private Integer id;
+
+    @NotBlank(message = "Title is required!")
+    private String title;
+    @NotBlank(message = "Genre is required!")
+    private String genre;
+
+
+    private LocalDate releaseDate;
+
+    private Double rating;
+
+    private String duration;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+}
